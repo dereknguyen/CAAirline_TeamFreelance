a7ada77ac23b22801ca776a70df9ade84e7500b6
isabel-0000 <blueribbonij02@gmail.com>
2018-05-08 16:01:39 -0700

    FlightManager.java

--- Code/src/com/company/FlightManager.java
+++ Code/src/com/company/FlightManager.java
@@ -1,13 +1,47 @@
 package com.company;
 
-public class FlightManager{
-   public void editStatus(){
+import java.util.Scanner;
 
+public class FlightManager{
+   Scanner reader = new Scanner(System.in);
+
+   public void editStatus(){
+      Database_Interface db = Database_Interface.getInstance();
+      Trip trip = getTripFromUser();
+      Date date = getDateFromUser();
+      int status;
+      int flightID;
+      
+      System.out.println("Enter status");
+      status = reader.nextInt();
+      
+      db.setStatus(date,trip, status);
+   }
+   public Trip getTripFromUser(){
+      Trip trip = new Trip();
+      System.out.println("Enter destination");
+      int destination = reader.nextInt();
+      trip.setDestination(destination);
+      return trip;
+   }
+   public Date getDateFromUser(){
+      Date date = new Date();
+      System.out.println("Enter date");
+      int d = reader.nextInt();
+      date.setDate(d);
+      return date;
    }
    public void viewStatus(){
-
+      Database_Interface db = Database_Interface.getInstance();
+      Trip trip = getTripFromUser();
+      Date date = getDateFromUser();
+      int status = db.getStatus(date, trip);
+      System.out.println(status);
    }
    public void scheduleFlight(){
-
+      Database_Interface db = Database_Interface.getInstance();
+      Trip trip = getTripFromUser();
+      Date date = getDateFromUser();
+      db.addFlight(flightID, date, trip);
    }
 }
