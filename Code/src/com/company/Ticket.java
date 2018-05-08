ddd794cc74c9056c79d7b5c488547186aba3c63a
sleeplikemad <bobby_liu999@yahoo.com>
2018-05-08 15:51:44 -0700

    ticket and trip classes added

---
+++ Code/src/com/company/Ticket.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Date;
+
+
+public class Ticket {
+	private Date date;
+	private Trip trip;
+	private int ticketNumber;
+	private int planeNumber;
+}
---
+++ Code/src/com/company/Trip.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Trip {
+	private String destination;
+}
