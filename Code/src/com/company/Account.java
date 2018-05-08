4b23a4b3e90a4cb5b8548e5cd73aede3f926deb7
Derek Nguyen <derek.nguyen.t@gmail.com>
2018-05-08 16:09:12 -0700

    Added Employee and Account class

---
+++ Code/src/com/company/Account.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Arrylist;
+
+public class Account {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private ArrayList<Ticket> ticket;
+
+    public Account(String firstname, String lastName, String username, ArrayList<Ticket> ticket) {
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.userName = username;
+        this.ticket = ticket;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String getAccount() {
+        return null;
+    }
+
+    public Ticket getTicket(int confirmNum) {
+        return nill;
+    }
+
+    public FlightStatus viewFlightStatus(int flightNum) {
+        return nill;
+    }
+
+    public boolean purchaseTicket(Date date, Trip trip) {
+        return false;
+    }
+
+    public String getUserName() {
+        return null;
+    }
+
+
+
+}
