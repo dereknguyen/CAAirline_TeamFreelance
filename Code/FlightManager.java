bb606ba13b7170bb5134ffa60f11734fc6d5cf20
isabel-0000 <blueribbonij02@gmail.com>
2018-05-09 16:32:45 -0700

    working with Date object

--- Code/FlightManager.java
+++ Code/FlightManager.java
@@ -9,40 +9,45 @@ public class FlightManager{
    public void editStatus(){
       Database_Interface db = Database_Interface.getInstance();
       Trip trip = getTripFromUser();
-      Date date = getDateFromUser();
       int status;
       int flightID;
       
       System.out.println("Enter status");
       status = reader.nextInt();
       
-      db.setStatus(date,trip, status);
+      db.setStatus(trip, status);
    }
    public Trip getTripFromUser(){
-      Trip trip = new Trip();
       System.out.println("Enter destination");
       int destination = reader.nextInt();
-      trip.setDestination(destination);
-      return trip;
-   }
-   public Date getDateFromUser(){
-      Date date = new Date();
-      System.out.println("Enter date");
+      System.out.println("Enter year");
+      int y = reader.nextInt();
+      System.out.println("Enter month");
+      int m = reader.nextInt();
+      System.out.println("Enter date"):
       int d = reader.nextInt();
-      date.setDate(d);
-      return date;
+      System.out.println("Enter hrs");
+      int h = reader.nextInt();
+      System.out.println("Enter min");
+      int m = reader.nextInt()
+      Date date = new Date(y,m,d,h,m);
+      Trip trip = new Trip(date,destination)
+      return trip;
    }
    public void viewStatus(){
       Database_Interface db = Database_Interface.getInstance();
       Trip trip = getTripFromUser();
-      Date date = getDateFromUser();
-      int status = db.getStatus(date, trip);
+      int status = db.getStatus(trip);
       System.out.println(status);
    }
    public void scheduleFlight(){
       Database_Interface db = Database_Interface.getInstance();
       Trip trip = getTripFromUser();
-      Date date = getDateFromUser();
-      db.addFlight(date, trip);
+      Date date = trip.date;
+      Date minH = new Date(date.getTime()-40*60*1000); //40 min in ms
+      Date maxH = new Date(date.getTime()+40*60*1000);
+      if(db.getNumFlights(minH, maxH)==0){
+         db.addFlight(trip);
+      }
    }
 }
