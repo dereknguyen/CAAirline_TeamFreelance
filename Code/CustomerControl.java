a59fe8e8291ef4b8e5902115af34ed1fe9f3b5d4
marinamoore <mmoore32@calpoly.edu>
2018-05-10 12:50:23 -0700

    destinationid

--- Code/CustomerControl.java
+++ Code/CustomerControl.java
@@ -4,9 +4,13 @@ import java.lang.System;
 import java.util.Scanner;
 
 public class CustomerControl{
-   static private CustomerControl uniqueinstance;
+   private Database_Interface db;
+   private static CustomerControl uniqueinstance;
 
-   private CustomerControl(){}
+   private  CustomerControl(){
+      db = Database_Interface.getInstance();
+   }
+
    // Flight Reserve
 
    static public CustomerControl getInstance()
@@ -19,29 +23,32 @@ public class CustomerControl{
    }
 
    public void reserve(String username){
+      FlightManager fm = new FlightManager();
       Scanner scan = new Scanner(System.in);
       Database_Interface db = Database_Interface.getInstance();
-      Trip trip = FlightManager.getTripFromUser();
-      
+      Trip trip = fm.getTripFromUser();
+
       int flightId = db.getFlightId(trip.getDest(), trip.getDate());
+      int flightId = 0;
       System.out.println("What seat would you like? Enter a number 1-20");
       int seatNo = scan.nextInt();
-      db.addTicket(username, flightId, seatNo);
+      db.reserveSeat(username, flightId, seatNo);
    }
-
    // Checkin
    public void checkin(String username){
+      FlightManager fm = new FlightManager();
       Database_Interface db = Database_Interface.getInstance();
-      Trip trip = FlightManager.getTripFromUser();
+      Trip trip = fm.getTripFromUser();
 
-      int flightId = db.getFlightId(trip.getDest(), trip.getDate());
+      //int flightId = db.getFlightId(trip.getDest(), trip.getDate());
+      int flightId = 0;
       db.flightCheckIn(username, flightId);
    }
-
    // View status
    public void status(){
+      FlightManager fm = new FlightManager();
       Database_Interface db = Database_Interface.getInstance();
-      Trip trip = FlightManager.getTripFromUser();
+      Trip trip = fm.getTripFromUser();
 
       System.out.println("Status" + db.getStatus(trip.getDest(), trip.getDate()));
    }
