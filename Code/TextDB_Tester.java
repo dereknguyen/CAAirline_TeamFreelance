d5e8d9a4074234465df49b2d07a5b9c0f15d827a
nparra1997 <nparra1997@gmail.com>
2018-05-15 17:08:07 -0700

    remove() functions working, possibly need more testing with nonstandard files and nonexistant entries

--- Code/TextDB_Tester.java
+++ Code/TextDB_Tester.java
@@ -14,30 +14,37 @@ public class TextDB_Tester
         Date d1 = new Date(c.getTime().getTime());
         Date d2 = new Date(c2.getTime().getTime());
 
-
-        // overwrites first entry
         db.addFlight(5, d1);
         db.addFlight(3, d2);
-
-        db.addTicket("nparra", db.getFlightId(5, d1), 15);
-        db.addTicket("nparra2", db.getFlightId(5, d1), 16);
-        db.addTicket("nparra3", db.getFlightId(3, d2), 2);
-
-        db.addEmployeeAccount("emp1", "Joe", "Smith");
-        db.addEmployeeAccount("emp2", "Bryan", "Jones");
 
         db.addCustomerAccount("nparra", "Nick", "Parra");
         db.addCustomerAccount("nparra2", "Hi", "World");
         db.addCustomerAccount("nparra3", "Hello", "Again");
 
-        // Correctly returns valid flight ID and -1 for nonexistent
-        //System.out.println(db.getFlightId(5, d));
-        //System.out.println(db.getFlightId(4,d));
+        db.addEmployeeAccount("emp1", "Joe", "Smith");
+        db.addEmployeeAccount("emp2", "Bryan", "Jones");
 
-        // Currently removes all lines from file, not working
-        /*db.removeEmployee("emp1");
+        db.addTicket("nparra", db.getFlightId(5, d1), 15);
+        db.addTicket("nparra2", db.getFlightId(5, d1), 16);
+        db.addTicket("nparra3", db.getFlightId(3, d2), 2);
+
+        System.out.println(db.getFlightId(5, d1)); // should be 0
+        System.out.println(db.getFlightId(3,d1)); // should be -1
+        System.out.println(db.getNumFlights(null, null)); // should be 2
+        System.out.println(db.getStatus(db.getFlightId(5, d1))); // should be 0
+
+        db.removeFlight(0);
+        db.removeFlight(1);
+
+        db.removeCustomer("nparra2");
         db.removeCustomer("nparra");
-        db.removeTicket("nparra", db.getFlightId(5, d));
-        db.removeFlight(db.getFlightId(5, d));*/
+        db.removeCustomer("nparra3");
+
+        db.removeEmployee("emp1");
+        db.removeEmployee("emp2");
+
+        db.removeTicket("nparra3", 3);
+        db.removeTicket("nparra2", 5);
+        db.removeTicket("nparra", 5);
     }
 }
