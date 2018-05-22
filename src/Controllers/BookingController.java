44dbcde5d3bc137034fa2ee219ace8bc50d2894b
nparra1997 <nparra1997@gmail.com>
2018-05-22 15:30:39 -0700

    added to src package and created new tests package

--- src/Controllers/BookingController.java
+++ src/Controllers/BookingController.java
@@ -1,4 +1,4 @@
-package src.Controllers;
+package Controllers;
 
 public class BookingController {
 }
--- src/Controllers/LoginCustomerController.java
+++ src/Controllers/LoginCustomerController.java
@@ -1,4 +1,4 @@
-package src.Controllers;
+package Controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
