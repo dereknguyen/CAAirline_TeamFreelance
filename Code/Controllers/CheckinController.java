86f5dbc604341d03e2bf6bb079a1de29d8c9a37c
marinamoore <mmoore32@calpoly.edu>
2018-06-01 10:51:30 -0700

    checkin backend

---
+++ Code/Controllers/CheckinController.java
@@ -0,0 +1,75 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import src.Database;
+import src.SQL_Database;
+
+import java.io.IOException;
+
+
+public class CheckinController {
+    @FXML
+    private TextField confirmationNumber;
+
+    @FXML
+    private Button CheckInButton;
+
+    @FXML
+    private TextField carryOn;
+
+    @FXML
+    private TextField checked;
+
+    @FXML
+    private Button confirmationButton;
+
+    @FXML
+    private Label errMsg;
+
+    @FXML
+    void CheckInButtonClick(ActionEvent event) throws IOException {
+        Database db = SQL_Database.getInstance();
+
+        //this assumes confirmation number is the trip id
+        Integer tripId = Integer.parseInt(confirmationNumber.getText().trim());
+
+        //TODO: get username
+        String username = "test";
+
+        db.checkIn(username, tripId);
+    }
+
+    @FXML
+    void ConfirmationButtonClick(ActionEvent event) {
+        String tempTripId = confirmationNumber.getText().trim();
+
+        if (tempTripId.equals("")) {
+            errMsg.setText("Please enter a confirmation number");
+            errMsg.setVisible(true);
+        }
+        else {
+            Integer tripId = Integer.parseInt(tempTripId);
+            Database db = SQL_Database.getInstance();
+
+
+            //TODO: get username
+            String username = "test";
+
+
+            int numBags = 0;
+            if (!carryOn.getText().trim().equals("")) {
+                numBags += Integer.parseInt(carryOn.getText().trim());
+            }
+            if (!checked.getText().trim().equals("")) {
+                numBags += Integer.parseInt(checked.getText().trim());
+            }
+
+
+            db.setBags(username, tripId, numBags);
+        }
+    }
+}
