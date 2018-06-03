54e7dc8da9436016e9b007d4fb6751289500e72a
Isabel-0000 <Isabel-0000@users.noreply.github.com>
2018-06-02 21:33:23 -0700

    fixes to trip

---
+++ Code/src/Trip.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Calendar;
+
+public class Trip {
+	private int TripId;
+	private int FlightId;
+	private Calendar Date;
+	private double Price;
+	private int Status;
+
+	public Trip(int TripId, int FlightId, Calendar Date, double Price, int Status)
+	{
+		this.TripId=TripId;
+		this.FlightId=FlightId;
+		this.Date=Date;
+		this.Price=Price;
+		this.Status=Status;
+	}
+
+	public Calendar getDate() { return Date; }
+
+	public int getTripId() { return TripId; }
+	public int getFlightId() { return FlightId; }
+}
