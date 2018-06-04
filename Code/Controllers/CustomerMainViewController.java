b252714b84ec48f7266abf13501ddcd30283f9d2
nparra1997 <nparra1997@gmail.com>
2018-06-04 12:16:53 -0700

    fixed typo

--- Code/Controllers/CustomerMainViewController.java
+++ Code/Controllers/CustomerMainViewController.java
@@ -205,7 +205,7 @@ public class CustomerMainViewController {
             B_FromCol.setCellValueFactory(new PropertyValueFactory<>("FromString"));
             B_ToCol.setCellValueFactory(new PropertyValueFactory<>("ToString"));
             B_DepartDateCol.setCellValueFactory(new PropertyValueFactory<>("DateString"));
-            B_ReturnDateCol.setCellValueFactory(new PropertyValueFactory<>("RT_DateString"));
+            B_ReturnDateCol.setCellValueFactory(new PropertyValueFactory<>("RTDateString"));
             B_PriceCol.setCellValueFactory(new PropertyValueFactory<>("Price"));
 
             B_AvailableFlightsTable.setItems(results);
